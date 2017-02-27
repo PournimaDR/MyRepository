04de53c2acf975d9bf7df8a5740e07328121c236
pournima <email>
2017-02-27 18:02:27 +0530

    hello

---
+++ SAOPServiceProducer123/src/com/mccoy/service/SimpleHelloworldProgram.java
@@ -0,0 +1,11 @@
+package com.mccoy.service;
+
+public class SimpleHelloworldProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
